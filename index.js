bf95c74e40dac30988de7b72749e720e227ffd32
mscbr <scibor.maciej@gmail.com>
2021-11-15 00:28:42 +0100

    ws: init

--- index.js
+++ index.js
@@ -3,24 +3,44 @@ require('dotenv').config();
 const services = require('./src/services');
 const logger = services.getLogger();
 
+const WS = require('./src/api/websocket/server');
+
 const Exchanges = require('./src/exchanges');
 const Arbitrage = require('./src/arbitrage');
 
 async function main() {
   const logger = services.getLogger();
-  const exchanges = new Exchanges(logger);
-  await exchanges.init();
-  const arbitrage = new Arbitrage();
   const tickerInterval = services.getInterval();
 
-  tickerInterval.setInterval(30, async () => {
+  const websocket = new WS(logger);
+  websocket.startServer();
+
+  const exchanges = new Exchanges(logger);
+  await exchanges.init();
+  const arbitrage = new Arbitrage(logger);
+
+  tickerInterval.setInterval(5, async () => {
     const tickers = await exchanges.fetchMarketTickers();
-    console.log('ARBS', arbitrage.scanAllMarketTickers({ tickers }));
+    const arbs = arbitrage.scanAllMarketTickers({ tickers });
+    websocket.emitData(
+      JSON.stringify(
+        arbs.arbs.map(
+          ({ market, profit, lowestAsk: { exchange: askExchange }, highestBid: { exchange: bidExchange } }) => ({
+            market,
+            profit,
+            askExchange,
+            bidExchange,
+          }),
+        ),
+      ),
+    );
+
+    // console.log('ARBS', arbitrage.scanAllMarketTickers({ tickers }));
   });
 
-  setTimeout(() => {
-    tickerInterval.terminateInterval();
-  }, 40000);
+  // setTimeout(() => {
+  //   tickerInterval.terminateInterval();
+  // }, 40000);
 }
 
 main();
